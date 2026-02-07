5240db4406fb62e4b84717df3326c85667804bd6
lohitha-java <uggulohitha2004@gmail.com>
2026-02-07 19:36:03 +0530

    Create Student.java

---
+++ src/Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    int id;
+    String name;
+    String email;
+    String course;
+
+    public Student(int id, String name, String email, String course) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+}
